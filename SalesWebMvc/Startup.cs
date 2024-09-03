234f1356dbdbe348245c40e84da9730ad51b5e15
Paulo Castelo <oluap.castelo@gmail.com>
2024-09-03 01:08:25 -0300

    Updating project to Net8.0

---
+++ SalesWebMvc/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting; // Atualizado para IWebHostEnvironment
+using SalesWebMvc.Data;
+using SalesWebMvc.Services;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+namespace SalesWebMvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Configuração de controllers com views e compilação em tempo de execução
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 39));
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), serverVersion));
+
+            services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordsService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService) // Atualizado para IWebHostEnvironment
+        {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                seedingService.Seed();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
